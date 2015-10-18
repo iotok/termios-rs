dd1a55e89a8fcbbac2cba6ceccc7b187eeaeaea7
Tae-il Lim <trlim@iotok.kr>
2015-10-18 21:37:05 +0900

    Add Android support

--- src/os/mod.rs
+++ src/os/mod.rs
@@ -1,9 +1,11 @@
 //! OS-specific definitions.
 
 #[cfg(target_os = "linux")] pub use self::linux as target;
+#[cfg(target_os = "android")] pub use self::linux as target;
 #[cfg(target_os = "macos")] pub use self::macos as target;
 #[cfg(target_os = "freebsd")] pub use self::freebsd as target;
 
 #[cfg(target_os = "linux")] pub mod linux;
+#[cfg(target_os = "android")] pub mod linux;
 #[cfg(target_os = "macos")] pub mod macos;
 #[cfg(target_os = "freebsd")] pub mod freebsd;
